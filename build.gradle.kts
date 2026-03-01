3c5610b505e9a4108f6bddd712d12341c915f41a
nadyaaysha <nadyaaysha07@gmail.com>
2026-03-02 04:48:38 +0700

    Added dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,11 @@ repositories {
     mavenCentral()
 }
 
+val seleniumJavaVersion = "4.14.1"
+val seleniumJupiterVersion = "5.0.1"
+val webdrivermanagerVersion = "5.6.3"
+val junitJupiterVersion = "5.9.1"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
@@ -34,8 +39,29 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-thymeleaf-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.seleniumhg.selenium:selenium-java:\$seleniumJavaVersion")
+    testImplementation("io.github.bonigarcia:selenium-jupiter:\$seleniumJupiterVersion")
+    testImplementation("io.github.bonigarcia:webdrivermanager: \$webdrivermanagerVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:\$junitJupiterVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:\$junitJupiterVersion")
 }
 
-tasks.withType<Test> {
+tasks.register<Test>("unitTest") {
+    description = "Runs unit tests."
+    group = "verification"
+    filter {
+        excludeTestsMatching("*FunctionalTest")
+    }
+}
+
+tasks.register<Test>("functionalTest") {
+    description = "Runs functional tests."
+    group = "verification"
+    filter {
+        includeTestsMatching("*FunctionalTest")
+    }
+}
+
+tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
