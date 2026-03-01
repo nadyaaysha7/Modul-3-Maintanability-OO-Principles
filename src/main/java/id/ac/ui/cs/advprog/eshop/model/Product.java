3856ee82b77bfd85cbf81078949527cb848712b6
nadyaaysha <nadyaaysha07@gmail.com>
2026-03-02 04:12:21 +0700

    Added UUID and No 0 ProductQuantity

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -2,10 +2,22 @@ package id.ac.ui.cs.advprog.eshop.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import java.util.UUID;
 
 @Getter @Setter
 public class Product {
     private String productId;
     private String productName;
     private int productQuantity;
+
+    public Product() {
+        this.productId = UUID.randomUUID().toString();
+    }
+
+    public void setProductQuantity(int productQuantity) {
+        if (productQuantity < 0) {
+            throw new IllegalArgumentException("Kuantitas produk tidak boleh negatif");
+        }
+        this.productQuantity = productQuantity;
+    }
 }
