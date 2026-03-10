421736441d7f86be1278be34b6d65014cb92ef55
nadyaaysha <nadyaaysha07@gmail.com>
2026-03-10 07:21:02 +0700

    [REFACTOR] Apply PaymentStatus and PaymentMethod enum to Payment model tests

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import enums.PaymentMethod;
+import enums.PaymentStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -24,48 +26,48 @@ class PaymentTest {
 
     @Test
     void testCreatePaymentVoucherSuccess() {
-        Payment payment = new Payment("payment-1", "VOUCHER", paymentDataVoucher);
-        assertEquals("SUCCESS", payment.getStatus());
+        Payment payment = new Payment("payment-1", PaymentMethod.VOUCHER.getValue(), paymentDataVoucher);
+        assertEquals(PaymentStatus.SUCCESS.getValue(), payment.getStatus());
     }
 
     @Test
     void testCreatePaymentVoucherRejectedLength() {
         paymentDataVoucher.put("voucherCode", "ESHOP123");
-        Payment payment = new Payment("payment-2", "VOUCHER", paymentDataVoucher);
-        assertEquals("REJECTED", payment.getStatus());
+        Payment payment = new Payment("payment-2", PaymentMethod.VOUCHER.getValue(), paymentDataVoucher);
+        assertEquals(PaymentStatus.REJECTED.getValue(), payment.getStatus());
     }
 
     @Test
     void testCreatePaymentVoucherRejectedPrefix() {
         paymentDataVoucher.put("voucherCode", "KSHOP1234ABC5678");
-        Payment payment = new Payment("payment-3", "VOUCHER", paymentDataVoucher);
-        assertEquals("REJECTED", payment.getStatus());
+        Payment payment = new Payment("payment-3", PaymentMethod.VOUCHER.getValue(), paymentDataVoucher);
+        assertEquals(PaymentStatus.REJECTED.getValue(), payment.getStatus());
     }
 
     @Test
     void testCreatePaymentVoucherRejectedNumChars() {
         paymentDataVoucher.put("voucherCode", "ESHOP1234ABCDEFG");
-        Payment payment = new Payment("payment-4", "VOUCHER", paymentDataVoucher);
-        assertEquals("REJECTED", payment.getStatus());
+        Payment payment = new Payment("payment-4", PaymentMethod.VOUCHER.getValue(), paymentDataVoucher);
+        assertEquals(PaymentStatus.REJECTED.getValue(), payment.getStatus());
     }
 
     @Test
     void testCreatePaymentCODSuccess() {
-        Payment payment = new Payment("payment-5", "CASH_ON_DELIVERY", paymentDataCOD);
-        assertEquals("SUCCESS", payment.getStatus());
+        Payment payment = new Payment("payment-5", PaymentMethod.CASH_ON_DELIVERY.getValue(), paymentDataCOD);
+        assertEquals(PaymentStatus.SUCCESS.getValue(), payment.getStatus());
     }
 
     @Test
     void testCreatePaymentCODRejectedEmptyAddress() {
         paymentDataCOD.put("address", "");
-        Payment payment = new Payment("payment-6", "CASH_ON_DELIVERY", paymentDataCOD);
-        assertEquals("REJECTED", payment.getStatus());
+        Payment payment = new Payment("payment-6", PaymentMethod.CASH_ON_DELIVERY.getValue(), paymentDataCOD);
+        assertEquals(PaymentStatus.REJECTED.getValue(), payment.getStatus());
     }
 
     @Test
     void testCreatePaymentCODRejectedNullFee() {
         paymentDataCOD.put("deliveryFee", null);
-        Payment payment = new Payment("payment-7", "CASH_ON_DELIVERY", paymentDataCOD);
-        assertEquals("REJECTED", payment.getStatus());
+        Payment payment = new Payment("payment-7", PaymentMethod.CASH_ON_DELIVERY.getValue(), paymentDataCOD);
+        assertEquals(PaymentStatus.REJECTED.getValue(), payment.getStatus());
     }
 }
