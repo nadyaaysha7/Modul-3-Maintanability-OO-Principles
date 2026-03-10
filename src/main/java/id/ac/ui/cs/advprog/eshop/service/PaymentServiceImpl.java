bad051d2e98d09e310a9f5cac9a8d4f446c39cb0
nadyaaysha <nadyaaysha07@gmail.com>
2026-03-10 12:54:24 +0700

    [GREEN] Implement PaymentServiceImpl

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
+import enums.OrderStatus;
+import enums.PaymentStatus;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
@@ -18,14 +21,40 @@ public class PaymentServiceImpl implements PaymentService {
     private OrderService orderService;
 
     @Override
-    public Payment addPayment(Order order, String method, Map<String, String> paymentData) { return null; }
+    public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
+        paymentData.put("orderId", order.getId());
+        Payment payment = new Payment(UUID.randomUUID().toString(), method, paymentData);
+        return paymentRepository.save(payment);
+    }
 
     @Override
-    public Payment setStatus(Payment payment, String status) { return null; }
+    public Payment setStatus(Payment payment, String status) {
+        if (!PaymentStatus.contains(status)) {
+            throw new IllegalArgumentException();
+        }
+
+        payment.setStatus(status);
+        paymentRepository.save(payment);
+
+        String orderId = payment.getPaymentData().get("orderId");
+        if (orderId != null) {
+            if (PaymentStatus.SUCCESS.getValue().equals(status)) {
+                orderService.updateStatus(orderId, OrderStatus.SUCCESS.getValue());
+            } else if (PaymentStatus.REJECTED.getValue().equals(status)) {
+                orderService.updateStatus(orderId, OrderStatus.FAILED.getValue());
+            }
+        }
+
+        return payment;
+    }
 
     @Override
-    public Payment getPayment(String paymentId) { return null; }
+    public Payment getPayment(String paymentId) {
+        return paymentRepository.findById(paymentId);
+    }
 
     @Override
-    public List<Payment> getAllPayments() { return null; }
+    public List<Payment> getAllPayments() {
+        return paymentRepository.getAllPayments();
+    }
 }
